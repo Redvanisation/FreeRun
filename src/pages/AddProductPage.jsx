d21872144c0f249f42576a5a2f4964baad7a5b94
Redvan <redvanisation@gmail.com>
2020-04-16 18:40:21 -0400

    Add authorization to add products and update products pages

--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import CreateProduct from '../components/CreateProduct';
+import { UserContext } from '../containers/UsersProvider';
 
-const AddProductPage = () => (
-  <Layout title="Add Product">
-    <CreateProduct />
-  </Layout>
-);
+const AddProductPage = () => {
+  const userCtx = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userCtx.cookies.user || !userCtx.cookies.user.admin) {
+      history.push('/');
+    }
+  });
+
+
+  return (
+    <Layout title="Add Product">
+      <CreateProduct />
+    </Layout>
+  );
+};
 
 export default AddProductPage;
--- src/pages/UpdateProductPage.jsx
+++ src/pages/UpdateProductPage.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import UpdateProduct from '../components/UpdateProduct';
+import { UserContext } from '../containers/UsersProvider';
 
-const UpdateProductPage = ({ location }) => (
-  <Layout title="Add Product">
-    <UpdateProduct location={location} />
-  </Layout>
-);
+const UpdateProductPage = ({ location }) => {
+  const userCtx = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!userCtx.cookies.user || !userCtx.cookies.user.admin) {
+      history.push('/');
+    }
+  });
+
+  return (
+    <Layout title="Add Product">
+      <UpdateProduct location={location} />
+    </Layout>
+  );
+};
 
 UpdateProductPage.propTypes = {
   location: PropTypes.instanceOf(Object).isRequired,
